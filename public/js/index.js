3b50dc27d792b49bb4884163860a49a7de3ae205
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-06 17:54:00 -0800

    Added sticky navbar

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+const body = document.body
+const nav = document.querySelector(".page-header nav")
+const menu = document.querySelector(".page-header .menu")
+const scrollUp = "scroll-up"
+const scrollDown = "scroll-down"
+let lastScroll = 0
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp)
+    body.classList.add(scrollDown)
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown)
+    body.classList.add(scrollUp)
+  }
+  lastScroll = currentScroll
+})
